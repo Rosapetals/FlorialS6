1985ff9241ec2dfdaa19cb9ffd11d4638e70d2f0
ChefAsmith <131435079+ChefAsmith@users.noreply.github.com>
2023-08-14 13:29:09 -0400

    Fix players able to set walk speed, removed giving players permission to speed.* in RankFlySpeedCommand

--- src/main/java/net/florial/commands/ranks/RankFlySpeedCommand.java
+++ src/main/java/net/florial/commands/ranks/RankFlySpeedCommand.java
@@ -20,11 +20,8 @@ public class RankFlySpeedCommand extends BaseCommand {
 
         if (rate > 5) return;
 
-        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "lp user " + p.getName() + " permission set essentials.*");
-
         Bukkit.getScheduler().runTaskLater(Florial.getInstance(), () -> {
-            Bukkit.dispatchCommand(p, "flyspeed " + rate);
-            Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "lp user " + p.getName() + " permission unset essentials.*");
+            Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "speed fly " + rate + " " + p.getName());
         }, 40L);
 
         p.sendMessage(CC.translate("#ffd7dc&l&nF#ffb8c1&l&nl#ff99a6&l&no#ff7a8b&l&nr#ff5b70&l&ni#ff3c55&l&na#ff1d3a&l&nl&r #ff3c55&l➤&f Set your fly speed to " + rate));
